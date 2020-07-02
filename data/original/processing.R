8cad3184aa60dfb94648aa7dce01cdd90feba972
Emma James <emljames@users.noreply.github.com>
2020-07-02 10:39:55 +0100

    upload data

---
+++ data/original/processing.R
@@ -0,0 +1,21 @@
+# Load libraries
+library(tidyverse)
+
+# Read in participant information (valid only) & picture naming data
+ppt_info <- read.csv("./data/original/backgroundInfo.csv") %>% 
+  filter(validData == 1)
+pic_name <- read.csv("./data/original/picName.csv") %>% 
+  filter(task == "PNT", time == 1 | time == 2)
+
+# Select relevant descriptive variables
+ppt_info <- select(ppt_info, ID, sex, WASI.vocab.raw)
+
+# Merge datasets together, keep relevant columns only
+full_data <- left_join(ppt_info, pic_name) %>% 
+  select(ID, sex, WASI.vocab.raw, learnTime, time, item, acc, RT) %>% 
+  set_names(c("ID", "sex", "vocab_score", "sleep_wake", "session", "item", "acc", "RT")) %>% 
+  mutate(sleep_wake = ifelse(sleep_wake == "PM", "sleep", "wake"))
+
+
+# Write csv file
+write.csv(full_data, "./data/AMPM_subset.csv", row.names = FALSE)
